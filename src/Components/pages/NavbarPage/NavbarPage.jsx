5348d26ed8accf47c42228b7c46f24b9f17f77f8
Diego <diegoguatedb2002@gmail.com>
2023-06-04 18:07:07 -0600

    Animacion de navbar corregida

--- src/Components/pages/NavbarPage/NavbarPage.jsx
+++ src/Components/pages/NavbarPage/NavbarPage.jsx
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { gsap } from 'gsap';
 import Navbar from '../../Navbar/Navbar';
 import ProjectsPage from '../ProjectsPage/ProjectsPage';
 import styles from './NavbarPage.module.css';
@@ -10,33 +9,14 @@ function NavbarPage() {
   const navbarRef = useRef();
   const pageContainerRef = useRef();
 
-  const [showContent, setShowContent] = useState(false);
-
-  const loadAnimation = () => {
-    const tl = gsap.timeline();
-
-    tl.fromTo(
-      navbarRef.current,
-      { css: { top: '-50px' } },
-      { css: { top: '0px', duration: 1 } },
-    ).eventCallback('onComplete', () => {
-      // mostrar contenido del router
-      setShowContent(true);
-    });
-  };
-
-  useEffect(() => {
-    loadAnimation();
-  }, []);
   return (
     <div className={styles.pageContainer} ref={pageContainerRef}>
       <Navbar refProp={navbarRef} />
-      {showContent && (
       <Routes>
         <Route path="/projects/*" element={<ProjectsPage />} />
         <Route path="/detail" element={<ProjectDetailPage />} />
       </Routes>
-      )}
+
     </div>
   );
 }
