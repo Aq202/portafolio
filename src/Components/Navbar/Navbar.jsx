13d82735d3579a011d9989815d8871e99d396312
Diego <diegoguatedb2002@gmail.com>
2023-06-04 23:02:05 -0600

    Navlink agregada en navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar({ refProp }) {
   return (
     <nav className={styles.navbar} ref={refProp}>
-      <Link to="/" className={styles.logoContainer}>
+      <NavLink to="/" className={styles.logoContainer}>
         <div className={styles.logo} />
         <span className={styles.logoName}>Diego Aquino</span>
-      </Link>
+      </NavLink>
       <ul>
-        <li><Link to="/yo">Sobre mí</Link></li>
-        <li><Link to="/portafolio">Portafolio</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to="/yo">
+            Sobre mí
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to="/portafolio">
+            Portafolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to="/contacto">
+            Contacto
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
